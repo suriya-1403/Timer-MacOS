40ff616ff855baa4ce711714198ccddcc30db4d6
SuriyaKrishnan <suriya@outlook.in>
2021-06-19 17:04:13 +0530

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by SuriyaKrishnan on 19/06/21.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
