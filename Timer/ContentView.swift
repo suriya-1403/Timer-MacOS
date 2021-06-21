3b89ba5debb33cf7dad1c19d43f788e16fc23c49
SuriyaKrishnan <suriya@outlook.in>
2021-06-21 15:39:57 +0530

    Added the Keyboard Shortcut to Play/Pause

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -9,20 +9,41 @@ import SwiftUI
 import AVKit
 import AVFoundation
 
-let defaultTimeRemaining: CGFloat = 9
+let defaultTimeRemaining: CGFloat = 900
 let lineWith:CGFloat = 25
 let radius:CGFloat = 50
 var player: AVAudioPlayer!
 
+struct NeumorphicButtonStyle: ButtonStyle {
+    var bgColor: Color
+
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .padding(20)
+            .background(
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .shadow(color: .white, radius: configuration.isPressed ? 7: 10, x: configuration.isPressed ? -5: -15, y: configuration.isPressed ? -5: -15)
+                        .shadow(color: .black, radius: configuration.isPressed ? 7: 10, x: configuration.isPressed ? 5: 15, y: configuration.isPressed ? 5: 15)
+                        .blendMode(.overlay)
+                    RoundedRectangle(cornerRadius: 10, style: .continuous)
+                        .fill(bgColor)
+                }
+        )
+            .scaleEffect(configuration.isPressed ? 0.95: 1)
+            .foregroundColor(.primary)
+            .animation(.spring())
+    }
+}
+
 struct ContentView: View {
     
     @State private var isActive = false
+//    static let space: KeyEquivalent
+//    Space (U+0020)
     @State private var timeRemaining:CGFloat = defaultTimeRemaining
     @State var audioPlayer: AVAudioPlayer!
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-//    let sound = Bundle.main.path(forResource: "Reflection", ofType: "m4r")
-//    self.audioPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
-    
     var body: some View {
         VStack(spacing: 25){
             HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 25, content: {
@@ -40,20 +61,60 @@ struct ContentView: View {
                     .font(.largeTitle)
             }.frame(width: radius*2, height: radius*2, alignment: .center)
             HStack(spacing: 25){
-                Label("\(isActive ? "Pause" :"Play")",systemImage: "\(isActive ? "pause.fill" :"play.fill")")
-                    .foregroundColor(isActive ? .purple : .red)
-                    .font(.body).onTapGesture {
+                ZStack{
+                    Button(action: {
                         AudioServicesPlaySystemSound(1026)
                         isActive.toggle()
-                        
+                        debugPrint("Button Pressed")
+                    }){}
+                    .padding(0)
+                    .opacity(0)
+                    .frame(width: 0, height: 0)
+                    .keyboardShortcut(.space,modifiers: [])
+                    Button(action: {
+                        AudioServicesPlaySystemSound(1026)
+                        isActive.toggle()
+                    }){
+                        HStack{
+                            Image(systemName: "\(isActive ? "pause.fill" :"play.fill")")
+                            Text("\(isActive ? "Pause" :"Play")")
+                        }
+                    }
+                    .foregroundColor(isActive ? .purple: .red)
+                    .font(.body)
+                    .buttonStyle(PlainButtonStyle())
                 }
-                Label("Resume",systemImage: "backward.fill")
-                    .foregroundColor(.yellow)
-                    .font(.body).onTapGesture {
+                ZStack{
+                    Button(action: {
+                        AudioServicesPlaySystemSound(1026)
+                        isActive.toggle()
+                        debugPrint("Button Pressed")
+                    }){}
+                    .padding(0)
+                    .opacity(0)
+                    .frame(width: 0, height: 0)
+                    .keyboardShortcut("s",modifiers: [])
+                    Button(action: {
                         AudioServicesPlaySystemSound(1026)
                         isActive = false
                         timeRemaining = defaultTimeRemaining
+                    }){
+                        HStack{
+                            Image(systemName: "backward.fill")
+                            Text("Resume")
+                        }
+                    }
+                    .foregroundColor(.yellow)
+                    .font(.body)
+                    .buttonStyle(PlainButtonStyle())
                 }
+//                Label("Resume",systemImage: "backward.fill")
+//                    .foregroundColor(.yellow)
+//                    .font(.body).onTapGesture {
+//                        AudioServicesPlaySystemSound(1026)
+//                        isActive = false
+//                        timeRemaining = defaultTimeRemaining
+//                }
             }
 
         }.onReceive(timer, perform: { _ in
